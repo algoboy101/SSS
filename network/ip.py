896cff80a7393a5693900990c6a4d46b24f1a3f5
Xuezhi.Zhang <algoboy@126.com>
2019-12-07 21:25:30 +0800

    添加 判断IP 和 域名转IP的功能

--- network/ip.py
+++ network/ip.py
@@ -5,6 +5,26 @@
 # @File : test.py
 import urllib.request
 # import ipdb
+import re
+import os
+
+
+def is_ip(server="127.0.0.1"):
+    pattern = re.compile(
+        '^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
+    if pattern.match(server):
+        return True
+    else:
+        return False
+
+
+def domain2ip(server="www.baidu.com"):
+    ip = os.popen("ping -c 1 %s |grep 'time=' | awk '{print $4}' |cut -b 1-" %
+                  server).readline().strip()
+    # 被墙的域名，无法获取它的IP地址
+    if not ip:
+        ip = None
+    return ip
 
 
 def get_ip_public():
@@ -19,7 +39,7 @@ def get_ip_public():
     return ip
 
 
-def get_ip_info():
+def get_ip_info(ip="127.0.0.1"):
     """
         返回公网IP的（国家，省，市，运营商）
         
@@ -37,7 +57,7 @@ def get_ip_info():
         
     :return: (country, province, city, ISP)
     """
-    ip = get_ip_public()
+    # ip = get_ip_public()
     if ip is None or len(ip) == 0:
         return None
     else:
@@ -87,8 +107,31 @@ def get_ip_info_taobo():
     # ip_info_list = ip_info.split(",")
 
 
-if __name__ == "__main__":
-    ip_info = get_ip_info()
+def _test_get_ip_info():
+    print("============== 测试 获取公网IP 和 获取公网IP信息 =============")
+    ip = get_ip_public()
+    ip_info = get_ip_info(ip)
     print(ip_info)
+
+
+def _test_domain2ip():
+    print("============== 测试 域名转IP =============")
+    res = domain2ip("www.baidu.com")
+    print("www.baidu.com >> ", res)
+
+
+def _test_is_ip():
+    print("============== 测试 输入是否是IP =============")
+    res = is_ip("wwww.baidu.com")
+    print("www.baidu.com is ip? >> ", res)
+    res = is_ip("39.156.66.18")
+    print("39.156.66.18 is ip? >> ", res)
+
+
+if __name__ == "__main__":
+    _test_get_ip_info()
+    _test_domain2ip()
+    _test_is_ip()
+
     # ip_info = get_ip_info_taobo()
     # print(ip_info)
