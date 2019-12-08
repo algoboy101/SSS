74b75db3f4c7833ef239003b6f6587ff884f9582
Xuezhi.Zhang <algoboy@126.com>
2019-12-08 10:33:19 +0800

    删除所有代码

--- convert/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time : 12/6/19 1:57 PM
-# @Author : xuezhi.zhang
-# @File : __init__.py.py
-
-from .dict import dict2url_ssr, dict2url_ss, dict2qrcode
-from .url import url2dict, url2qrcode
-from .qrcode import qrcode2url, qrcode2dict
